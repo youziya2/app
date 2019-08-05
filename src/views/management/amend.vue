6816943720f6fe21ca7db14ef4bb8613d18e08ed
youziya2 <53429487+youziya2@users.noreply.github.com>
2019-08-05 10:27:02 +0800

    Update amend.vue

--- src/views/management/amend.vue
+++ src/views/management/amend.vue
@@ -71,28 +71,9 @@
 			},
 			methods: {
 				submitForm(formName) {
-					// console.log(formName)
-					var that = this
-					let token = JSON.parse(sessionStorage.getItem("token")).access_token
-					token = "Bearer" + " " + token
-					this.axios.defaults.headers.common['Authorization'] = token;
-					let uid=JSON.parse(sessionStorage.getItem("token")).profile.userUid
-	// 				console.log(uid)
-	// 				console.log(that.ruleForm.age)
-	// 				console.log(that.ruleForm.pass)
-					this.axios.get('http://192.168.1.188:12/api/User/ModifyPassword?uid='+uid+'&oldPassword='+that.ruleForm.age+'&newPassword='+that.ruleForm.pass+'')
-						.then(function(res) {
-							sessionStorage.clear();
-							that.$router.push({name:"login"})
-							that.$message({
-								message: '恭喜你，修改成功',
-								type: 'success'
-							});
-						})
-						.catch(function(error) {
-							console.log(error);
-						});
+
 				},
+				//重置方法
 				resetForm(formName) {
 					this.$refs[formName].resetFields();
 				}
